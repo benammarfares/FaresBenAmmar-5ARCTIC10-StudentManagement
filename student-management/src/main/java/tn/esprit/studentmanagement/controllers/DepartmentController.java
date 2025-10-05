19114bd3cd83f0ce760e22bff9148cb03cb5e80c
Fares <benammarfaroutiti@gmail.com>
2025-10-05 15:20:44 +0100

    test (addded  space) in code to test trigger

--- student-management/src/main/java/tn/esprit/studentmanagement/controllers/DepartmentController.java
+++ student-management/src/main/java/tn/esprit/studentmanagement/controllers/DepartmentController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/Depatment")
 @CrossOrigin(origins = "http://localhost:4200")
+
 @AllArgsConstructor
 public class DepartmentController {
     private IDepartmentService departmentService;
